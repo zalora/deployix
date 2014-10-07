dde4d65b8cd7f4e3964ebc8d9950fa429caf7efb
Shea Levy <shea@shealevy.com>
2014-10-07 14:46:04 -0400

    php-fpm: Just take the config and ini as arguments

--- defnixos/services/php-fpm.nix
+++ defnixos/services/php-fpm.nix
@@ -4,45 +4,29 @@ activations@{ socket }:
 
 pkgs@{ multiplex-activations, execve, php }:
 
-let
-
-  ini = "${php}/etc/php-recommended.ini";
-
-  /* TODO: Figure out logging */
-  config = pool-name: builtins.toFile "php-fpm.conf" ''
-    [global]
-    daemonize = no
-    error_log = syslog
-
-    [${pool-name}]
-    listen = /run/phpfpm/${pool-name}.sock
-    slowlog = syslog
-    user = root
-    group = root
-    pm = static
-    pm.max_children = 10
-  '';
-
-in
-
 # A php-fpm pool
 
-{ pool-name # The name of the fpm pool
+{ socket-path # The path to the fpm socket
+, config # the php-fpm config
+, ini ? "${php}/etc/php-recommended.ini" # php.ini
+, user ? "nginx" # user to run as (php-fpm most often used with nginx)
 }:
 
 {
   start = multiplex-activations (execve "start-php-fpm" {
     filename = "${php}/sbin/php-fpm";
 
-    argv = [ "php-fpm" "-R" "--fpm-config" (config pool-name) "-c" ini ];
+    argv = [ "php-fpm" "--fpm-config" config "-c" ini ];
 
     envp = {
-      FPM_SOCKETS = "/run/phpfpm/${pool-name}.sock=3";
+      FPM_SOCKETS = "${socket-path}=3";
     };
+
+    inherit user;
   }) [ (socket {
     family = lib.socket-address-families.AF_UNIX;
 
-    path = "/run/phpfpm/${pool-name}.sock";
+    path = socket-path;
   }) ];
 
   on-demand = true;
