22e42530e98f7a2a464f7a41c174a6497efba289
Shea Levy <shea@shealevy.com>
2014-10-07 14:42:46 -0400

    execve: Support for switching user

--- pkgs/execve/default.nix
+++ pkgs/execve/default.nix
@@ -1,10 +1,12 @@
-lib: lib.composable [ "build-support" "pkgs" ] (
+lib: lib.composable [ "eval-support" "build-support" "pkgs" ] (
+
+eval-support@{ calculate-id }:
 
 build-support@{ compile-c, write-file }:
 
 pkgs@{ coreutils }:
 
-name: { filename, argv, envp ? null }: let
+name: { filename, argv, envp ? null, user ? null }: let
   needs-envp = envp != null;
 
   exec-fun = if needs-envp
@@ -16,6 +18,10 @@ name: { filename, argv, envp ? null }: let
       ''"${name}=${value}"''
     ) envp) ++ [ "NULL" ])} };
   '' else "";
+
+  setuid = if user == null
+    then "0"
+    else "setuid(${calculate-id user})";
 in compile-c [ "-Wl,-s" ] (write-file "${name}.c" ''
   #include <unistd.h>
   #include <err.h>
@@ -30,6 +36,8 @@ in compile-c [ "-Wl,-s" ] (write-file "${name}.c" ''
   ${envp-def}
 
   int main(int argc, char ** _argv) {
+    if (${setuid} == -1)
+      err(213, "Setting user id");
     ${exec-fun};
     err(212, "executing %s", filename);
   }
