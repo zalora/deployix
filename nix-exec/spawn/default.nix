1b9a1b306b7a8dbf6bc62950df00d0eb7e7fef48
Shea Levy <shea@shealevy.com>
2014-11-17 12:54:55 -0500

    Fix plugin linking on darwin

--- nix-exec/spawn/default.nix
+++ nix-exec/spawn/default.nix
@@ -8,9 +8,6 @@ defnix: let
     "-fPIC"
     "-I${nix}/include/nix"
     "-I${boehmgc}/include"
-    "-lnixformat"
-    "-lnixexpr"
-    "-lnixutil"
-    "-L${nix}/lib"
+    "-undefined" "dynamic_lookup"
   ] ./spawn.so.cc;
 in defnix.lib.nix-exec.dlopen so "nix_spawn" 2
