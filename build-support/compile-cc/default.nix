fc281473c84c59f835e2e89315506ca950703620
Shea Levy <shea@shealevy.com>
2014-11-17 00:08:25 -0500

    Syntax error

--- build-support/compile-cc/default.nix
+++ build-support/compile-cc/default.nix
@@ -11,12 +11,12 @@ flags: cc: let
 
   base-flags = [ "-Wall" "-Werror" "-O3" "-std=c++11" "-o" "@out" ];
 
-  compile-strip-and-patchelf = compile-c [
+  compile-strip-and-patchelf = compile-c ([
     "-DCOMPILER=\"${cxx}\""
   ] ++ (if target-system == "x86_64-darwin" then [] else [
     "-DPATCHELF=\"${patchelf}/bin/patchelf\""
     "-DSTRIP=\"${binutils}/bin/strip\""
-  ]) ../compile-c/compile-strip-and-patchelf.c;
+  ])) ../compile-c/compile-strip-and-patchelf.c;
 in output-to-argument (derivation {
   name = builtins.substring 0 (builtins.stringLength base - 3) base;
 
