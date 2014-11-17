710178f2f1eeca6c35a9eef70c019b169f937207
Shea Levy <shea@shealevy.com>
2014-11-17 00:33:48 -0500

    Need all syms resolved for DSOs on darwin

--- nix-exec/spawn/default.nix
+++ nix-exec/spawn/default.nix
@@ -8,5 +8,9 @@ defnix: let
     "-fPIC"
     "-I${nix}/include/nix"
     "-I${boehmgc}/include"
+    "-lnixformat"
+    "-lnixexpr"
+    "-lnixutil"
+    "-L${nix}/lib"
   ] ./spawn.so.cc;
 in defnix.lib.nix-exec.dlopen so "nix_spawn" 2
