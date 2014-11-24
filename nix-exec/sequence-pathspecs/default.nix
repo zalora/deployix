7e437e71f59f1e5f12150b887de2fee94ba688e1
Shea Levy <shea@shealevy.com>
2014-11-23 21:56:40 -0500

    sequence-pathspecs: Allow specifying the full URL to the repo

--- nix-exec/sequence-pathspecs/default.nix
+++ nix-exec/sequence-pathspecs/default.nix
@@ -3,7 +3,8 @@ defnix: let
   inherit (lib) map-attrs nix-exec;
   inherit (nix-exec) sequence-attrs;
   inherit (nix-exec.builtins) fetchgit;
-in pathspecs: sequence-attrs (map-attrs (name: { repo, rev }: fetchgit {
-  url = "git://github.com/${repo}.git";
-  inherit rev;
-}) pathspecs)
+in pathspecs: sequence-attrs (map-attrs (name:
+  { repo ? null, url ? "git://github.com/${repo}.git", rev }: fetchgit {
+    inherit rev url;
+  }
+) pathspecs)
