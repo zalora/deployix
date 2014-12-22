78d4d7fe40d5de6020c8d7519150fb24ffd89e86
Shea Levy <shea@shealevy.com>
2014-12-22 10:50:26 -0500

    Add a build-support function to run a series of tests

--- build-support/uncomposed.nix
+++ build-support/uncomposed.nix
@@ -16,4 +16,25 @@ lib: (lib.recursive-import ./.) // {
 
   # Haskell compiler
   ghc = defnix: "${defnix.nixpkgs.haskellPackages.ghcPlain}/bin/ghc";
+
+  # Run a series of tests
+  write-test-script = defnix: let
+    inherit (defnix.pkgs) sh;
+
+    inherit (defnix.build-support) write-script;
+
+    inherit (defnix.lib) join map-attrs-to-list;
+  in tests: write-script "tests" ''
+    #!${sh} -e
+
+    failed=
+    ${join "\n" (map-attrs-to-list (test-name: test-command: ''
+      echo Running test ${test-name} >&2
+      ${test-command} || failed=1
+    '') tests)}
+    if [ -n "$failed" ]; then
+      echo At least one test failed >&2
+      exit 1
+    fi
+  '';
 }
