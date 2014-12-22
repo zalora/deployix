e148c3dab0732145c9ea5bb41dbe39c1063c65fb
Shea Levy <shea@shealevy.com>
2014-12-22 16:50:58 -0500

    decrypt-secrets-dir: Enable taking multiple dirs

--- nix-exec/decrypt-secrets-dir/README.md
+++ nix-exec/decrypt-secrets-dir/README.md
@@ -8,7 +8,9 @@ Arguments
 
 * `secrets-dir`: The directory of encrypted secrets. There must be two files for
   each secret, one encrypted and ending in `.gpg` and one in the clear with
-  dummy values for use in development/testing
+  dummy values for use in development/testing. If a list, each element is
+  treated as a directory, with earlier elements taking precedence in case of
+  duplicates.
 * `decrypt`: Whether to actually decrypt the secrets or just used the cleartext
   dummy files
 
--- nix-exec/decrypt-secrets-dir/default.nix
+++ nix-exec/decrypt-secrets-dir/default.nix
@@ -1,13 +1,13 @@
-defnix: secrets-dir: let
+defnix: secrets-dirs: let
   # !!! Clean up when we have https://github.com/NixOS/nix/commit/976df480c918f050608f7a23a4a21415c43475c3
-  secret-files = builtins.filter (x:
+  secret-files = dir: builtins.filter (x:
     x != null
   ) (defnix.lib.map-attrs-to-list (name: type:
     if builtins.stringLength name >= 4 &&
       builtins.substring ((builtins.stringLength name) - 4) 4 name == ".gpg"
       then null
       else name
-  ) (builtins.readDir secrets-dir));
+  ) (builtins.readDir dir));
 
   inherit (defnix.lib.nix-exec) sequence unit bind;
 
@@ -17,15 +17,20 @@ defnix: secrets-dir: let
     then getpass "Enter passphrase for deployment key: "
     else unit null);
 
-  io-decrypted = bind io-pass (pass: sequence (map (secret-file:
-    defnix.lib.nix-exec.map (value:
-      { name = secret-file; inherit value; }
-    ) (decrypt-file pass secret-file (secrets-dir + "/${secret-file}.gpg"))
-  ) secret-files));
+  dirs = if builtins.isList secrets-dirs
+    then secrets-dirs
+    else [ secrets-dirs ];
+  io-decrypted = bind io-pass (pass: sequence (builtins.concatLists (map (dir:
+    map (secret-file:
+      defnix.lib.nix-exec.map (value:
+        { name = secret-file; inherit value; }
+      ) (decrypt-file pass secret-file (dir + "/${secret-file}.gpg"))
+    ) (secret-files dir)
+  ) dirs)));
 
-  io-cleartext = unit (map (secret-file:
-    { name = secret-file; value = secrets-dir + "/${secret-file}"; }
-  ) secret-files);
+  io-cleartext = unit (builtins.concatLists (map (dir: map (secret-file:
+    { name = secret-file; value = dir + "/${secret-file}"; }
+  ) (secret-files dir)) dirs));
 in decrypt: defnix.lib.nix-exec.map builtins.listToAttrs (if decrypt
   then io-decrypted
   else io-cleartext
