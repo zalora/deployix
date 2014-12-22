ea84ce10004d8f7db9ef5b7379a1e33127a124d4
Shea Levy <shea@shealevy.com>
2014-12-22 11:51:01 -0500

    spawn sets argv[0] to prog

--- nix-exec/uncomposed.nix
+++ nix-exec/uncomposed.nix
@@ -2,9 +2,9 @@ lib: (lib.recursive-import ./.) // {
   # Spawn a program, throwing if it fails
   spawn-successful = defnix: prog: argv: lib.nix-exec.map ({ signalled, code }:
     if signalled
-      then throw "${builtins.head argv} killed by signal ${toString code}"
+      then throw "${prog} killed by signal ${toString code}"
     else if code != 0
-      then throw "${builtins.head argv} exited with non-zero code ${
+      then throw "${prog} exited with non-zero code ${
         toString code
       }"
       else null
